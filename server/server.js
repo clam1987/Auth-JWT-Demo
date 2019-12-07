1f9b7a703de1c1e8efef29227c19e9d13d6bfcc3
Wilson Lam <lin-1987@live.com>
2019-12-06 19:14:17 -0800

    Created More Initial Files and Started Server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+// Node Packages
+const express = require("express");
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const dotenv = require("dotenv");
+const passport = require("passport");
+
+// Global Variables
+const PORT = process.env.PORT || 3001;
+
+// Initialize DotEnv
+dotenv.config();
+
+// DB Config
+// const db = require("./config/connection");
+
+// BodyParser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Routes
+
+
+// Connect To Server
+app.listen(PORT, () => {
+console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+PORT,
+PORT)
+});
